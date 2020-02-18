13d87d675038c5091d7a76d214bf0194b20095b4
Mark Newman <mwnewman@umich.edu>
2020-02-18 09:59:05 -0500

    final push Tuesday lecture

--- scrapehello.py
+++ scrapehello.py
@@ -3,7 +3,7 @@
 
 from bs4 import BeautifulSoup
 
-f = open("hello3.html")
+f = open("hello4.html")
 html_text = f.read()
 soup = BeautifulSoup(html_text, 'html.parser')
 # print(soup.prettify())
@@ -21,48 +21,48 @@ all_french_list_items = soup.find_all('li', class_='french')
 # searching by id
 all_hello_elements = soup.find_all(id='hello-list')
 
-print('list items:', all_list_items)
+print("RESULTS OF find_all()")
+print("soup.find_all('li'):", all_list_items)
+print("------")
+print("soup.find_all('div'):", all_divs)
 print('------')
-print('divs:', all_divs)
-print('------')
-print('goodbye elements:', all_goodbye_elements)
-print('------')
-print('french stuff:', all_french_list_items)
-print('------')
-print('hello id elements:', all_hello_elements)
-print('------')
+print("soup.find_all('li', class_='french'):", all_french_list_items)
+print("------")
+print("soup.find_all(id='hello-list'):", all_hello_elements)
+print("------")
 
+
+print("TYPE OF LIST ELEMENTS RETURNED BY find_all()")
 print(type(all_list_items[0]))
 print('------')
 
+print("EXTRACTING STRINGS FROM LIST ITEMS AND PRINTING THEM UPPERCASE")
 for li in all_list_items:
-    print(li.string, type(li.string))
-    print(li.string.upper())
-
+    print(li.string.upper(), type(li.string))
 print('------')
 
+print("EXAMINING THE CHILDREN OF THE 'HELLO' LIST")
 for child in all_hello_elements[0].contents:
     print(child)
     print(type(child))
     print('------')
 
-print('------')
+print("USING strip() TO GET RID OF WHITESPACE")
 print(all_hello_elements[0].contents[0].strip())
 print('------')
 
+print("ITERATING THROUGH ITEMS IN A LIST")
+hello_list_items = all_hello_elements[0].find_all('li')
+for item in hello_list_items:
+    print(item.text.strip())
 print('------')
+
+print("USING find() INSTEEAD OF find_all()")
 hello_div = soup.find(id='hello-list')
+print(hello_div)
 print('------')
 
-for child in hello_div.contents:
-    print(child)
-    print(type(child))
-    print('------')
-
-print('------')
-print(hello_div.contents[0].strip())
-print('------')
-
+print("ACCESSING TAG ATTRIBUTES")
 img_tag = soup.find('img')
 print('The img source:')
 print(img_tag['src'])
